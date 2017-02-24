45a60263927a6c5c222eeb0706366e46b2a44946
Jack Cassidy <j.cassidy45@gmail.com>
2017-02-24 15:16:13 +0000

    Dan's work on chatrooms brought in

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -25,41 +25,45 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.firebase.udacity.friendlychat.Sign_in.mUsername;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
 
-    public static final String ANONYMOUS = "anonymous";
-    public static final String JACK = "Jack";
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-
-    private ListView mMessageListView;
-    private MessageAdapter mMessageAdapter;
-    private ProgressBar mProgressBar;
-    private EditText mMessageEditText;
-    private Button mSendButton;
-    private Button mSignout;
 
     private String mUsername;
 
-    // Firebase database
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mMessagesDatabaseReference;
-    private ChildEventListener mChildEventListener;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private String name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,96 +72,50 @@ public class MainActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         mUsername = (String) extras.get(mUsername);
 
-        // References to Firebase realtime database.
-        // mFirebaseDatabase = access to root of database, mMessagesDatabaseReference = access to "messages" portion of database
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
 
-        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        listView = (ListView) findViewById(R.id.moduleListView);
 
-        // Initialize references to views
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mMessageListView = (ListView) findViewById(R.id.messageListView);
-        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
-        mSendButton = (Button) findViewById(R.id.sendButton);
-        mSignout = (Button) findViewById(R.id.sign_out_menu);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                list_of_rooms);
 
-        //mMessageListView.setVisibility(View.INVISIBLE);
+        listView.setAdapter(arrayAdapter);
+        final String modules[] = new String[]{"3C1", "3C2", "3D1", "3D2", "3D5B"};
 
-        // Initialize message ListView and its adapter
-        List<FriendlyMessage> friendlyMessages = new ArrayList<>();
-        List<String> chatroomList = new ArrayList<>();
 
-        mMessageAdapter = new MessageAdapter(this, R.layout.item_message, friendlyMessages);
-        mMessageListView.setAdapter(mMessageAdapter);
 
-        // Initialize progress bar
-        mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+       root.addListenerForSingleValueEvent(new ValueEventListener() {
+           @Override
+           public void onDataChange(DataSnapshot dataSnapshot) {
+               Set<String> set = new HashSet<String>();
+               Iterator i = dataSnapshot.getChildren().iterator();
 
-        // Enable Send button when there's text to send
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
+               while (i.hasNext()){// && isItemInArray(((DataSnapshot)i.next()).getKey(), modules, 5 )){
+                   set.add(((DataSnapshot)i.next()).getKey());
+               }
+               list_of_rooms.clear();
+               list_of_rooms.addAll(set);
+
+               arrayAdapter.notifyDataSetChanged();
+           }
+
+           @Override
+           public void onCancelled(DatabaseError databaseError) {
+
+           }
+       });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                Intent intent = new Intent(getApplicationContext(), ChatRoom.class);
+                intent.putExtra("room_name",((TextView)view).getText().toString() );
+                intent.putExtra("user_name", mUsername);
+                startActivity(intent);
             }
 
-            // Enables/Disables send button depending on if there are characters in the edit text field
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
-                } else {
-                    mSendButton.setEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
         });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
-
-        // Send button sends a message and clears the EditText
-        mSendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create a new message object with text from the EditText widget and attach the username to it. Null for photo url
-                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(), mUsername);
-                mMessagesDatabaseReference.push().setValue(friendlyMessage);
-                // Clear input box
-                mMessageEditText.setText("");
-            }
-        });
-
-        mChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class);
-                mMessageAdapter.add(friendlyMessage);
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        };
-        // Attach childEventListener to the "messages" section of the dbase
-        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
-
-
-
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -176,5 +134,14 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public boolean isItemInArray(String s, String[] mod, int size){
+        for(int i = 0; i< size;i++)
+        {
+            if(mod[i]==s)
+                return true;
+        }
+        return false;
     }
 }
