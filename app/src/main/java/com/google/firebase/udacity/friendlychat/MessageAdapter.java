78c83ba16818e604d30b2d8578edc14a4b735229
evebyrne <byrnee39@tcd.ie>
2017-03-17 15:02:03 +0000

    speech bubbles!

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
@@ -1,39 +1,107 @@
 package com.google.firebase.udacity.friendlychat;
 
-import android.app.Activity;
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-
+import java.util.ArrayList;
 import java.util.List;
 
-public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+import android.app.Activity;
+import android.content.Context;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
+
+
+public class MessageAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<FriendlyMessage> mMessages;
+
     public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
-        super(context, resource, objects);
+        super();
+        this.mContext = context;
+        this.mMessages = objects;
+    }
+    @Override
+    public int getCount() {
+        return mMessages.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mMessages.get(position);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        FriendlyMessage message = (FriendlyMessage) this.getItem(position);
+
+        ViewHolder    holder;
         if (convertView == null) {
-            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_message, parent, false);
+            holder = createViewHolder(convertView);
+            convertView.setTag(holder);
         }
+        else
+            holder = (ViewHolder) convertView.getTag();
 
-        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
-        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+       // TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        //TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+        LinearLayout.LayoutParams layoutParams =
+                (LinearLayout.LayoutParams) holder.contentWithBG.getLayoutParams();
+        layoutParams.gravity = Gravity.RIGHT;
+        holder.contentWithBG.setLayoutParams(layoutParams);
 
-        FriendlyMessage message = getItem(position);
+        RelativeLayout.LayoutParams lp =
+                (RelativeLayout.LayoutParams) holder.content.getLayoutParams();
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+        lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        holder.content.setLayoutParams(lp);
+        layoutParams = (LinearLayout.LayoutParams) holder.txtMessage.getLayoutParams();
+        layoutParams.gravity = Gravity.LEFT;
+        holder.txtMessage.setLayoutParams(layoutParams);
 
-        messageTextView.setVisibility(View.VISIBLE);
-        messageTextView.setText(message.getText());
-        authorTextView.setText(message.getName());
+        holder.txtMessage.setText(message.getText());
+        holder.contentWithBG.setBackgroundResource(R.drawable.patch);
+        //holder.msg.setTextColor(R.color.black);
+        //messageTextView.setVisibility(View.VISIBLE);
+        //messageTextView.setText(message.getText());
+        //TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+        //authorTextView.setText(message.getName());
 
         return convertView;
     }
 
+
+    private ViewHolder createViewHolder(View v) {
+        ViewHolder holder = new ViewHolder();
+        holder.txtMessage = (TextView) v.findViewById(R.id.messageTextView);
+        holder.contentWithBG = (LinearLayout) v.findViewById(R.id.contentWithBackground);
+        holder.content = (LinearLayout) v.findViewById(R.id.content);
+        return holder;
+    }
+
+    private static class ViewHolder {
+        public TextView txtMessage;
+        public LinearLayout contentWithBG;
+
+        public LinearLayout content;
+
+    }
+    public void add(FriendlyMessage message) {
+        mMessages.add(message);
+    }
+    @Override
+    public long getItemId(int position) {
+        //Unimplemented, because we aren't using Sqlite.
+        return position;
+    }
 }
 
