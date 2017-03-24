d0c8c1e731c1c3e22aad2326a1db6c3f3196e3f9
Jack Cassidy <j.cassidy45@gmail.com>
2017-03-24 16:36:17 +0000

    Now have upvoted being synced between devices

--- app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
@@ -132,6 +132,19 @@ public class ChatRoom extends AppCompatActivity {
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class);
+
+                // Loop to find position of old message, remove it and replace with the upvoted message
+                for(int i = 0; i < mMessageAdapter.getCount(); i++)
+                {
+                    if(friendlyMessage.getFbaseKey().equals(mMessageAdapter.getItem(i).getFbaseKey()))
+                    {
+                        FriendlyMessage oldMessage = mMessageAdapter.getItem(i);
+                        mMessageAdapter.remove(oldMessage);
+                        mMessageAdapter.insert(friendlyMessage, i);
+                        break;
+                    }
+                }
             }
 
             @Override
