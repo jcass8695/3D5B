dab22d4056969f86038013e6c2a6eb162f23c21b
obriea21 <obriea21@tcd.ie>
2017-03-31 18:44:02 +0100

    blah

--- app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
@@ -22,11 +22,15 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
