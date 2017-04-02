9cad3ff33d24f10056afe646b409df9cf6a02e36
obriea21 <obriea21@tcd.ie>
2017-04-02 04:21:10 +0100

    Update ChatRoom.java

--- app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
@@ -74,6 +74,23 @@ public class ChatRoom extends AppCompatActivity {
     private StorageReference mStorage;
     private ProgressDialog mProgress;
     private int mCounter;
+    
+    // Requesting permission to RECORD_AUDIO
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {android.Manifest.permission.RECORD_AUDIO};
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,6 +131,8 @@ public class ChatRoom extends AppCompatActivity {
         mFilename += "/recorded_audio_"+mCounter+".3gp";
         mStorage = FirebaseStorage.getInstance().getReference();
         mProgress = new ProgressDialog(this);
+        
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
 
         //Record Audio function
