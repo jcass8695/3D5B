731505df4438904e65b1a6ca89c17c6e10296605
obriea21 <obriea21@tcd.ie>
2017-04-02 05:05:27 +0100

    working with msg to display while recording

--- app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/ChatRoom.java
@@ -96,23 +96,6 @@ public class ChatRoom extends AppCompatActivity {
 
     }
 
-    // Requesting permission to RECORD_AUDIO
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {android.Manifest.permission.RECORD_AUDIO};
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
-        }
-        if (!permissionToRecordAccepted ) finish();
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
